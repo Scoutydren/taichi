d16c2c35f37f4a96e338ed98447e804613c36f0c
rgu <gubeini@seas.upenn.edu>
2021-07-26 19:06:09 +0800

    fix offset error

--- examples/rendering/hbao.py
+++ examples/rendering/hbao.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import numpy as np
 from numpy.core.fromnumeric import shape
+import taichi_glsl as ts
 
 import taichi as ti
 
-ti.init(arch=ti.gpu, kernel_profiler=True)
+ti.init(arch=ti.cpu, cpu_max_num_threads=1)
+# ti.init(arch=ti.gpu, kernel_profiler=True)
 res = 2048
 DIR_NUM = 16
 STEP_NUM = 8
@@ -14,15 +16,17 @@ INV_2PI = 1.0 / (2 * PI)
 
 in_tex = ti.field(ti.f32, shape=(res, res))
 out_tex = ti.field(ti.f32, shape=(res, res))
-mipmap_tex = ti.field(ti.f32, shape=(res, res * 3 // 2))
+mipmap_tex = ti.field(ti.f32, shape=(res, res))
 
 noise_tex = ti.Vector.field(3, ti.f32, shape=(16, 16))
 
 source = Image.open('source.png')
+source = source.rotate(-90)
 source_np = np.array(source) / 255.0
 source_np = np.float32(source_np)
 
 mipmap = Image.open('mipmap.png')
+mipmap = mipmap.rotate(-90)
 mipmap_np = np.array(mipmap) / 255.0
 mipmap_np = np.float32(mipmap_np)
 
@@ -100,10 +104,7 @@ def hbao_sd(radius: ti.f32, height_depth: ti.f32, src: ti.template(), mipmap: ti
         accum_ao = 0.0
         pixel_depth = src[P]
         for dir_id in ti.static(range(DIR_NUM)):
-            #may add some angle jittering
             dir_ang = dir_id * 2.0 * PI / DIR_NUM
-            # rand = noise_tex[P & 0x11]
-            # dir_ang = (dir_id + rand.x) * 2.0 * PI / DIR_NUM
             dir_vec = ti.Vector([ti.cos(dir_ang), ti.sin(dir_ang)])
             # for each step
             # substance designer use a power-exponent step size like cone tracing, rather than fixed step
@@ -111,19 +112,20 @@ def hbao_sd(radius: ti.f32, height_depth: ti.f32, src: ti.template(), mipmap: ti
             max_d = 0.0
             offset = 1
             offset_inv = 1.0
-            mipmap_offset = 0
             for mip_level in ti.static(range(1, STEP_NUM_SD + 1)):
                 sample_offset = offset * dir_vec
+                # S = P + sample_offset
                 S = ti.cast(P + sample_offset, int)
-                norm_S = S / res
+                S = S & (res - 1)
                 mip_res = res >> mip_level
-                x = ti.cast(mip_res * norm_S[0], int)
-                y = ti.cast(mip_res * norm_S[1], int)
-                sample_depth = mipmap[res + x, mipmap_offset + y]
+                mip_S = S / res * mip_res
+                x = ti.cast(mip_S[0], int)
+                y = ti.cast(mip_S[1], int)
+                sample_depth = mipmap[x & (res - 1), (mip_res + y) & (res - 1)]
+                # sample_depth = src[S[0] & 2047, S[1] & 2047]
                 # we upres offset for next level, deliberately upres before dividing depth because it's so in SD
                 offset = offset << 1
                 offset_inv = offset_inv * 0.5
-                mipmap_offset += mip_res
                 # we see it as depth contribution normalized by offset
                 d = (sample_depth - pixel_depth) * offset_inv * 0.5
                 max_d = ti.max(max_d, d)
@@ -138,28 +140,53 @@ def hbao_sd(radius: ti.f32, height_depth: ti.f32, src: ti.template(), mipmap: ti
             accum_ao += max_ao
         dst[P] = 1.0 - accum_ao / DIR_NUM
 
-# def halve_image(image):
-#     rows, cols = image.shape
-#     image = image.astype('uint16')
-#     image = image.reshape(rows // 2, 2, cols // 2, 2)
-#     image = image.sum(axis=3).sum(axis=1)
-#     return ((image + 2) >> 2).astype('uint8')
+@ti.func
+def sample_mipmap_func(src:ti.template(), dst:ti.template(), x, y, mip_level):
+    '''
+    x: x in 0-1
+    y: y in 0-1
+    '''
+    mip_size = 1024
+    for i in range(1, mip_level):
+        mip_size = mip_size >> 1
+    pos = ti.Vector([x,y])
+    i_pos = ti.cast(pos * 2048, ti.i32)
+    mip_pos = ti.cast(pos * mip_size, ti.i32)
+    dst[i_pos] = src[mip_pos.x, (mip_pos.y + mip_size)]
+    '''
+    1: 0
+    2: 1024
+    3: 1024 + 512
+    '''
 
-# def mipmap(image):
-#     img = image.copy()
-#     rows, cols = image.shape
-#     mipmap = np.zeros((rows, cols * 3 // 2), dtype='uint8')
-#     mipmap[:, :cols] = img
-#     row = 0
-#     while rows > 1:
-#         img = halve_image(img)
-#         rows = img.shape[0]
-#         mipmap[row:row + rows, cols:cols + img.shape[1]] = img
-#         row += rows
-#     return mipmap
+@ti.kernel
+def sample_mipmap(src:ti.template(), dst:ti.template(), mip_level:ti.i32):
+    for P in ti.grouped(src):
+        normalize_p = P / res
+        sample_mipmap_func(src, dst, normalize_p.x, normalize_p.y, mip_level)
 
-# img = np.array(Image.open('source.png'))
-# Image.fromarray(mipmap(img)).save('mipmap.png')
+
+def halve_image(image):
+    rows, cols = image.shape
+    image = image.astype('uint16')
+    image = image.reshape(rows // 2, 2, cols // 2, 2)
+    image = image.sum(axis=3).sum(axis=1)
+    return ((image + 2) >> 2).astype('uint8')
+
+def mipmap(image):
+    img = image.copy()
+    rows, cols = image.shape
+    mipmap = np.zeros((rows, cols), dtype='uint8')
+    row = 0
+    while rows > 1:
+        img = halve_image(img)
+        rows = img.shape[0]
+        mipmap[row:row + rows, :img.shape[1]] = img
+        row += rows
+    return mipmap
+
+img = np.array(Image.open('source.png'))
+Image.fromarray(mipmap(img)).save('mipmap.png')
 
 gui = ti.GUI('HBAO', res=(res, res))
 init_noise()
@@ -172,6 +199,7 @@ radius.value = 1.0
 # for i in range(1000):
 ti.kernel_profiler_print()
 while True:
+    # sample_mipmap(mipmap_tex, out_tex, 10)
     hbao_sd(height.value, radius.value, in_tex, mipmap_tex, out_tex)
     gui.set_image(out_tex.to_numpy())
     gui.show()
